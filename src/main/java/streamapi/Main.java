13bc8c6765b15aeced6e1dfd297a97771bd41df8
Enes <enesemrepohland@gmail.com>
2025-05-22 16:52:11 +0200

    Task iv+v: getResourceAsStream() und resources() mit Streams + 3 Imports: Collectors, StandartCharset,java.io.*

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -1,10 +1,9 @@
 package streamapi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -73,8 +72,8 @@ public class Main {
      * @return An open {@link InputStream} for the resource file
      */
     private static InputStream getResourceAsStream(String path) {
-        // TODO
-        throw new UnsupportedOperationException();
+        // "/" sorgt dafür, dass im Klassenpfad ab src/main/resources gesucht wird
+        return Main.class.getResourceAsStream("/" + path);
     }
 
     /**
@@ -88,31 +87,14 @@ public class Main {
      * @return String of all matching lines, separated by {@code "\n"}
      */
     public static String resources(String path) {
-        // TODO
-        StringBuilder result = new StringBuilder();
-
-        try (InputStream stream = getResourceAsStream(path)) {
-            BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-
-            List<String> allLines = new ArrayList<>();
-
-            String newLine = r.readLine();
-            while (newLine != null) {
-                allLines.add(newLine);
-                newLine = r.readLine();
-            }
-
-            for (int i = 1; i < allLines.size(); i++) {
-                String s = allLines.get(i);
-                if (s.startsWith("a") && !(s.length() < 2)) {
-                    result.append(allLines.get(i)).append("\n");
-                }
-            }
-
+        try (InputStream in = getResourceAsStream(path);
+             BufferedReader reader = new BufferedReader(
+                 new InputStreamReader(in, StandardCharsets.UTF_8))) {
+            return reader.lines()
+                .filter(line -> line.startsWith("a") && line.length() >= 2)
+                .collect(Collectors.joining("\n"));
         } catch (IOException e) {
-            System.err.println("Ouch, that didn't work: \n" + e.getMessage());
+            throw new UncheckedIOException(e);
         }
-
-        return result.toString();
     }
 }
